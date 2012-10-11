1e52fc954bc60364f024bf9951e5a22c8a571c97
Sam-Mauris Yong <mauris@hotmail.sg>
2012-10-11 14:58:16 +0800

    removed bootstrap file
    updated test bootstrapper

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,18 @@
 <?php
 
-include('src/Bootstrap.php');
-Packer\Bootstrap::initialize();
+set_include_path(realpath(__DIR__ . '/../src')
+        . PATH_SEPARATOR . get_include_path());
+spl_autoload_register(function($class) {
+    $class = ltrim($class, '\\');
+    $fileName  = '';
+    $namespace = '';
+    $lastNsPos = strrpos($class, '\\');
+    if($lastNsPos){
+        $namespace = substr($class, 0, $lastNsPos);
+        $class = substr($class, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace)
+                . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+    require_once($fileName);
+});
