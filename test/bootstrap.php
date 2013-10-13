8edda512fe6abed534d4632607864323e5b58339
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-13 12:11:07 +0800

    updated test bootstrap file to rely on composer autoload

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,10 +1,4 @@
 <?php
 
-spl_autoload_register(function($class) {
-    $class = ltrim($class, '\\');
-    $fileName  = realpath(__DIR__ . '/../src') . DIRECTORY_SEPARATOR;
-    $fileName .= str_replace(array('\\', '_'), DIRECTORY_SEPARATOR, $class) . '.php'; 
-    if(file_exists($fileName)){
-        require_once($fileName);
-    }
-});
+$loader = require(__DIR__ . '/../vendor/autoload.php');
+$loader->add('Packer\\', __DIR__);
